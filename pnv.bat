475316633752bf44dd275b3d8d5a681027381b72
fcharest <frederik.charest@radio-canada.ca>
2015-05-04 16:43:09 -0400

    better versionning

--- pnv.bat
+++ pnv.bat
@@ -1,14 +1,19 @@
 @echo off
 
-for /f %%i in ('git describe --abbrev=0 --tags') do set CURRENT=%%i
+SETLOCAL
+git describe --abbrev=0 --tags > ___temptemp.txt
+SET /p CURRENT=<___temptemp.txt
+del ___temptemp.txt
 
 set NEXTMINOR=%CURRENT:~-1%
 set /a INC=%NEXTMINOR%+1
 
 set version=%CURRENT:~0,-1%%INC%
+echo Upgrading to %version%
 
 git add . --all
 git commit -a -m %1
 git tag -a %version% -m %1
 git push
 git push --tags
+ENDLOCAL
